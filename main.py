bd3cc580ec25c4ca02f609be14b31ca6fd581daa
Jakub <sandomierzj@gmail.com>
2019-09-12 21:38:25 +0200

    Add selecting a track system

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ def check():
     for file in os.listdir(path):
         if file.endswith(".mp3"):
             print (trackNumber,". ", file, sep="")
+            trackList.append(file)
             trackNumber += 1
 
 
 print("Welcome in the music player!")
 print("List of tracks you have:")
 check()
+print (trackList)
+musicSelect = int(input("Select a track: "))
+while musicSelect > (trackNumber - 1) or musicSelect < 0:
+    print("Number out of range!")
+    musicSelect = int(input("Select a track: "))
